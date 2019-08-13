699af31381ce51119f5761d8f1c90bcf34328621
Barry Sheppard <barrysheppard@gmail.com>
2019-08-13 07:41:17 +0100

    Working through the questions

--- question03.R
+++ question03.R
@@ -9,12 +9,13 @@ rm(list = ls())
 setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
 # This installs all the packages needed if not already loaded
 if (!require("pacman")) install.packages("pacman")
-pacman::p_load('quantmod', 'ggplot2')
+pacman::p_load('quantmod', 'xts', 'ggplot2', 'tseries', 'forecast')
 
 # Question 3
 # Using Yahoo Finance API, select a specific stock market price,
 # apply time series analysis, consider ‘close price as your time
 # series variable:
+
 # (a) Validate the assumptions using graphical visualization.
 # (b) Fit the optimized model for ‘close price’ and provide
 #     the coefficient estimates for the fitted model.
@@ -28,18 +29,147 @@ pacman::p_load('quantmod', 'ggplot2')
 # Other time series code from this guide
 # https://rpubs.com/JSHAH/481706
 
-pbr <- getSymbols("GAW.L", src = "yahoo",
+# First we need to fetch the data from Yahoo Finance
+
+data <- getSymbols("GAW.L", src = "yahoo",
                   from = "2018-01-01", to = "2019-07-11",
                   auto.assign = FALSE)
 
-# Remove all but the closing price
-ts <- pbr[,4]
+head(data)
+dim(data)
 
-head(ts)
+# Remove all but the closing price
+stock_prices <- data[,4]
+# check for any NAs
+stock_prices[is.na(stock_prices)]
+
+# (a) Validate the assumptions using graphical visualization.
+
+# plot the data
+plot(stock_prices, type = 'l', main = 'log returns plot')
+abline(reg = lm( stock_prices ~ time(stock_prices)), col="blue")
+
+# From the plot we can see the mean and variance is not stationary
+
+# (b) Fit the optimized model for ‘close price’ and provide
+#     the coefficient estimates for the fitted model.
+
+auto.fit <- auto.arima(stock_prices, seasonal = TRUE)
+auto.fit
+
+# Series: stock_prices 
+# ARIMA(2,1,2) with drift 
+# 
+# Coefficients:
+#           ar1      ar2     ma1     ma2   drift
+#       -0.2825  -0.5965  0.2727  0.7108  5.8976
+# s.e.   0.2328   0.1838  0.2057  0.1600  3.7954
+# 
+# sigma^2 estimated as 5044:  log likelihood=-2185.08
+# AIC=4382.15   AICc=4382.38   BIC=4405.87
+
+# p,d,q are p = 2, d = 1, q = 2
+# x1 = -0.2825 * x_{t-1} +  -0.5965 * x_{t-2} + e_t * 
+
+
+# (c) What is the estimated order for AR and MA?
+
+
+pacf(stock_prices , lag.max=20)  
+acf(stock_prices , lag.max=100)   
+
+
+# We need to make this stationary 
+stock <- diff(log(stock_prices))
+plot(stock)
+
+
+# Series: stock_prices 
+# ARIMA(2,1,2) with drift 
+# 
+# Coefficients:
+#           ar1      ar2     ma1     ma2   drift
+#       -0.2825  -0.5965  0.2727  0.7108  5.8976
+# s.e.   0.2328   0.1838  0.2057  0.1600  3.7954
+# 
+# sigma^2 estimated as 5044:  log likelihood=-2185.08
+# AIC=4382.15   AICc=4382.38   BIC=4405.87
+
+
+# This is where we get the parameters from 
+# 
+
+auto.fcast <- forecast(auto.fit, h = 30)
+plot(auto.fcast)
+
+
+
+### Code from class
+
+
+library(quantmod) 
+library(xts) 
+
+
+# Pull data from Yahoo finance  
+data <- getSymbols("MSFT", src = "yahoo", from='2012-01-01',  
+                   to='2017-01-01', auto.assign = FALSE) 
+#data=na.omit(data) 
+head(data) 
+dim(data) 
+# Select the relevant close price series 
+stock_prices = data[,4] 
+class(stock_prices) 
+# Plot log returns  
+plot(stock_prices,type='l', main='log returns plot') 
+
+stock = diff(log(stock_prices)) 
+plot(stock,type='l', main='log returns plot') 
+
+stock = stock[!is.na(stock)] # remove missing values 
+
+acf( stock , lag.max=20)   
+pacf( stock , lag.max=20)  
+#d=1 
+#q=2 
+#p=1 
+manual.fit<-arima(stock, c(1,1,2))  # fitted model 
+manual.fcast <- forecast(manual.fit, h=10) # prediction for 10 step ahead 
+plot(manual.fcast) 
+#automodelling  
+
+auto.fit<-auto.arima(stock_prices, seasonal=F)  # automatic way to find the optimized model 
+auto.fit 
+auto.fcast <- forecast(auto.fit, h=30) 
+plot(auto.fcast) 
+
+
+#####
+
+
+
+
+as.ts(ts)
+head(as.ts(ts))
 describe(ts)
 str(ts)
 
 # (a) Validate the assumptions using graphical visualization.
+
+
+class(ts) # check the form of class
+# data is alreaday in the 'ts' time series format.
+# normally we would need to convert to time series using ts()
+head(ts)
+str(ts) # structure of data
+
+## First we check to see if the mean is stationary
+
+
+# To look at whether the mean is stationary we can plot it and plot the mean
+plot(ts)  # time series
+abline(reg = lm(ts~time(ts)), col="blue") # line for the mean of time series
+
 
 chart_Series(ts)
 
@@ -51,8 +181,8 @@ class(dts)
 # (b) Fit the optimized model for ‘close price’ and provide
 #     the coefficient estimates for the fitted model.
 
-auto.fit <- auto.arima(ts, seasonal=T)  # automatic way to find the optimized model
-tsdisplay(residuals(auto.fit), lag.max=45)
+auto.fit <- auto.arima(ts, seasonal = TRUE)  # automatic way to find the optimized model
+tsdisplay(residuals(auto.fit), lag.max = 45)
 summary(auto.fit)
 
 # Series: ts 
@@ -108,9 +238,9 @@ BIC(MA)
 
 # Based on these scores, MA is lower and probably a better fit
 
-# (d) Forecast h=10 step ahead prediction of wage on the plot
+# (d) Forecast h=10 step ahead prediction of close price on the plot
 #     of the original time series.
 
-auto.fcast <- forecast(auto.fit, h=10)
+auto.fcast <- forecast(auto.fit, h =  10)
 plot(auto.fcast)
 
--- question04.R
+++ question04.R
@@ -9,10 +9,8 @@ rm(list = ls())
 setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
 # This installs all the packages needed if not already loaded
 if (!require("pacman")) install.packages("pacman")
-pacman::p_load('psych', 'MASS', 'car')
+pacman::p_load('psych', 'MASS', 'car', 'ggplot2', 'GGally', 'CCA', 'sem', 'cluster')
 
-# Reference code from
-# https://rpubs.com/ifn1411/LDA
 
 # Question 4
 # Use dataset available on
@@ -22,6 +20,12 @@ df <- read.csv('http://users.stat.ufl.edu/~winner/data/nfl2008_fga.csv')
 names(df)
 head(df)
 describe(df)
+str(df)
+
+#There are two rows incomplete
+df[!complete.cases(data),]
+# As this is only 2 out of 1039 observations, we choose to exclude these
+data <- df[complete.cases(df),]
 
 # 1. Use LDA to classify the dataset into few classes so that
 #    at least 90% of information of dataset is explained through
@@ -29,11 +33,11 @@ describe(df)
 #    variables “togo”, “kicker”, and “ydline”). How many LDs do
 #    you choose? Explain the reason.
 
-qtrlda <- lda(qtr ~ togo + kicker + ydline, data = df)
+qtrlda <- lda(qtr ~ togo + kicker + ydline, data = data)
 qtrlda
 
 # Call:
-#   lda(qtr ~ togo + kicker + ydline, data = df)
+#   lda(qtr ~ togo + kicker + ydline, data = data)
 # 
 # Prior probabilities of groups:
 #   1          2          3          4          5 
@@ -57,15 +61,8 @@ qtrlda
 #   LD1   LD2   LD3 
 # 0.615 0.322 0.063 
 
-
-qtrldavalues <- predict(qtrlda)
-
-ldahist(qtrldavalues$x[,1], g = qtr)
-
-
-newdata <- data.frame(type = df[,1], lda = qtrldavalues$x)
-library(ggplot2)
-ggplot(newdata) + geom_point(aes(lda.LD1, lda.LD2, colour = type), size = 2.5)
+0.615 + 0.322 # 0.937
+# LD1 and LD2 combined explain 93.7% of the information and are selected
 
 
 # 2. Apply PCA, and identify the important principle components
@@ -73,10 +70,102 @@ ggplot(newdata) + geom_point(aes(lda.LD1, lda.LD2, colour = type), size = 2.5)
 #    decision strategy? Plot principle components versus their
 #    variance (Hint: to sketch the plot use the Scree plot).
 
+# We start by removing any of the non-continuous data
+# We also remove season as it has 0 variance
+keep_columns <- c('qtr', 'min', 'sec', 'down', 'togo','kicker','ydline','distance',
+                  'homekick', 'kickdiff', 'timerem', 'offscore', 'defscore',
+                  'GOOD', 'Missed', 'Blocked')
+data <- data[,keep_columns]
+
+names(data)
+describe(data)
+fit <- princomp(data, cor = TRUE)
+
+summary(fit)
+# Importance of components:
+#                           Comp.1    Comp.2    Comp.3     Comp.4     Comp.5     Comp.6
+# Standard deviation     2.0048406 1.6721636 1.3083384 1.22081818 1.11060318 1.00083350
+# Proportion of Variance 0.2512116 0.1747582 0.1069843 0.09314981 0.07708996 0.06260423
+# Cumulative Proportion  0.2512116 0.4259698 0.5329542 0.62610397 0.70319393 0.76579816
+#                            Comp.7     Comp.8     Comp.9    Comp.10    Comp.11     Comp.12
+# Standard deviation     0.97841761 0.95110310 0.88207039 0.82066687 0.62552037 0.203651798
+# Proportion of Variance 0.05983131 0.05653732 0.04862801 0.04209338 0.02445473 0.002592128
+# Cumulative Proportion  0.82562948 0.88216680 0.93079481 0.97288819 0.99734293 0.999935053
+#                             Comp.13      Comp.14      Comp.15      Comp.16
+# Standard deviation     3.223577e-02 3.041048e-08 2.446684e-08 8.914025e-09
+# Proportion of Variance 6.494657e-05 5.779982e-17 3.741415e-17 4.966240e-18
+# Cumulative Proportion  1.000000e+00 1.000000e+00 1.000000e+00 1.000000e+00
+
+loadings(fit)
+plot(fit, type = 'lines')
+biplot(fit)
+# From the screeplot we have a couple of options we could choose from
+# Comp1 + Comp2 + Comp3 explains 53% of the data and the increase to 4 is only 9%
+# Another possibile point of inflection could be 6 which explains 76% and the inrease
+# to 7 is only 6%
+
+# in this quesiton however, we're being asked to explain 90% of the dataset variation
+# for that we need a total of 9 components which explain 93%
+
+
+
 # 3. Split the dataset into two sets of variables so that
-#    X=( togo,kicker,ydline) and Y=( distance, homekick). Apply
+#    X=( togo,kicker,ydline) and Y=( distance, homekick).
+
+names(data)
+x <- data[,5:7]
+y <- data[,8:9]
+names(x)
+# [1] "togo"   "kicker" "ydline"
+names(y)
+# [1] "distance" "homekick"
+  
+#    Apply
 #    canonical correlation analysis to find the cross-correlation
 #    between X and Y. What is the correlation between ydline and
 #    distance?
+
+
+# display the canonical correlations
+cc1 <- cc(x, y)
+cc1$cor
+# [1] 0.99894989 0.06975549
+
+cor(x, y)  # correlation between two set of variables
+#            distance    homekick
+# togo    0.315641454 -0.04838438
+# kicker -0.001951722 -0.02363159
+# ydline  0.998947222  0.04295427
+
+
+# The correlation betweel ydline and distance is 0.998947222
+# So positively correlated and almost 1!
+
 #    Use K-means clustering analysis to identify the most
 #    important classes. How many classes do you select? Why?
+
+
+# We use this wssplot function is to work out the lowest number of clusters
+# with the highest amount of variation (information)
+wssplot <- function(data, nc=10, seed=1234){
+  wss <- (nrow(data)-1)*sum(apply(data,2,var))
+  for (i in 2:nc) {
+    set.seed(seed)
+    wss[i] <- sum(kmeans(data, centers = i)$withinss)}
+  plot(1:nc, wss, type = "b", xlab = "Number of Clusters",
+       ylab = "Within groups sum of squares")}
+
+wssplot(data, nc = 10) 
+# This scree plot gives us an indication of the number of clusters we should be
+# looking for. In this example, after 4 there is no major change.
+# We could also make a case for 3, but 4 appears to be a better point of inflection
+
+# generate the clusters
+k.means.fit <- kmeans(data, 4) # the 4 indicates the number of groups previous selected
+
+# We can also plot this to visualise the 4 groups
+library(cluster)
+clusplot(data, k.means.fit$cluster, main = '2D representation of the Cluster solution',
+         color = TRUE, shade = TRUE,
+         labels = 2, lines = 0)
+
